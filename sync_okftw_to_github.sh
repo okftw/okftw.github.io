8efd47d1f092f50721fb9c809d580afc2babcc88
okftw <okforthewin@gmail.com>
2025-01-31 00:00:05 +1100

    Update website - Fri 31 Jan 00:00:05 AEDT 2025

--- sync_okftw_to_github.sh
+++ sync_okftw_to_github.sh
@@ -5,22 +5,11 @@ cd /src/okftw.github.io
 git config --global user.email "okforthewin@gmail.com"
 git config --global user.name "okftw"
 
-# Commit changes locally
-git add .
-git commit -m "Update website - $(date)"
-
-#!/bin/bash
-
-cd /src/okftw.github.io
-
-git config --global user.email "okforthewin@gmail.com"
-git config --global user.name "okftw"
-
-# Commit changes locally
-git add .
-git commit -m "Update website - $(date)"
-
 git pull origin main
+
+# Commit changes locally
+git add .
+git commit -m "Update website - $(date)"
 
 git push origin main
 
